575f4ef46283acabe854f97678d384a57250cbe6
leeyeonji28 <yeon102800@gmail.com>
2022-07-06 11:26:04 +0900

    게시글 테이블 추가

--- db.sql
+++ db.sql
@@ -5,12 +5,12 @@ USE jpaBoard;
 
 # 회원 테이블 생성
 CREATE TABLE `user` (
-                        id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                        reg_date DATETIME NOT NULL,
-                        update_date DATETIME NOT NULL,
-                        email VARCHAR(100) NOT NULL,
-                        `password` VARCHAR(150) NOT NULL,
-                        `name` CHAR(50) NOT NULL
+    id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    reg_date DATETIME NOT NULL,
+    update_date DATETIME NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    `password` VARCHAR(150) NOT NULL,
+    `name` CHAR(50) NOT NULL
 );
 
 SHOW TABLES;
@@ -38,3 +38,35 @@ email = 'use3@test.com',
 `name` = '유저3';
 
 SELECT * FROM `user`;
+
+# 게시물 테이블 생성
+CREATE TABLE `article` (
+    id BIGINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    reg_date DATETIME NOT NULL,
+    update_date DATETIME NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    `body` TEXT NOT NULL,
+    `user_id` BIGINT UNSIGNED NOT NULL
+);
+
+# 게시물 데이터 생성
+INSERT INTO `article`
+SET reg_date = NOW(),
+update_date = NOW(),
+title = '제목 1',
+`body` = '내용 1',
+`user_id` = 1;
+
+INSERT INTO `article`
+SET reg_date = NOW(),
+update_date = NOW(),
+title = '제목 2',
+`body` = '내용 2',
+`user_id` = 2;
+
+INSERT INTO `article`
+SET reg_date = NOW(),
+update_date = NOW(),
+title = '제목 3',
+`body` = '내용 3',
+`user_id` = 3;
